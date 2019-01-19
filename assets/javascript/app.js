aee243090fbbee1ef8e23a80364019dc4467ea22
Joshua Beverly <beverlyj93@hotmail.com>
2019-01-19 11:33:21 -0600

    Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var config = {
     apiKey: "AIzaSyD-UOTfAohaCsTYKynpLJZ_Aq8SiePKVHE",
     authDomain: "bev-employeedata.firebaseapp.com",
@@ -10,27 +9,4 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.firestore();
-=======
-var rowNum = 1;
-$('#submit').on('click', function () {
-    var newRow = $('<div>').addClass('row').attr('id', 'row' + rowNum );
-    var targetRow = $('div');
-    var newColName = $('<div>').addClass('col-2').text($('#inputName.val()'));
-    targetRow.append(newColName);
-    var newColRole = $('<div>').addClass('col-2').text($('#inputRole.val()'));
-    targetRow.append(newColRole);
-    var newColStart = $('<div>').addClass('col-2').text($('#inputStart.val()'));
-    targetRow.append(newColStart);
-    //format date
-    var monthsWorked = ;
-    var newColMonths = $('<div>').addClass('col-2').text(monthsWorked);
-    targetRow.append(newColMonths);
-    var payRate = $('inputRate.val()')
-    var newColRate = $('<div>').addClass('col-2').text(payRate);
-    targetRow.append(newColRate);
-    var newColBilled = $('<div>').addClass('col-2').text(monthsWorked*payRate);
-    targetRow.append(newColBilled);
-    $('.table').append(newRow);
-    rowNum++;
-    });
->>>>>>> 30a8e106c5a7405a16f630c893c0b4892cdb00bf
+
