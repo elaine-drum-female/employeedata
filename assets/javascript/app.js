55e248b88e08334e97021113de89998d299e650d
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-01-19 10:55:03 -0600

    preparing the on-click for use

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,14 @@ var rowNum = 1;
 $('#submit').on('click', function () {
     var newRow = $('<div>').addClass('row').attr('id', 'row' + rowNum );
     var targetRow = $('div');
-    var newColName = $('<div>').addClass('col-2').text(#inputName.val());
+    var newColName = $('<div>').addClass('col-2').text($('#inputName.val()'));
     targetRow.append(newColName);
-    var newColRole = $('<div>').addClass('col-2').text(#inputRole.val());
+    var newColRole = $('<div>').addClass('col-2').text($('#inputRole.val()'));
     targetRow.append(newColRole);
-    var newColStart = $('<div>').addClass('col-2').text(#inputStart.val());
+    var newColStart = $('<div>').addClass('col-2').text($('#inputStart.val()'));
     targetRow.append(newColStart);
     //format date
-    var monthsWorked;
+    var monthsWorked = ;
     var newColMonths = $('<div>').addClass('col-2').text(monthsWorked);
     targetRow.append(newColMonths);
     var payRate = $('inputRate.val()')
