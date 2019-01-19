df16010d977916372126d40a1f34e90c61f8b60a
Joshua Beverly <beverlyj93@hotmail.com>
2019-01-19 12:08:11 -0600

    Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
+$(document).ready(() => {
+    refreshRows();
+})
+
 var config = {
     apiKey: "AIzaSyD-UOTfAohaCsTYKynpLJZ_Aq8SiePKVHE",
     authDomain: "bev-employeedata.firebaseapp.com",
@@ -10,11 +14,27 @@ firebase.initializeApp(config);
 
 var database = firebase.firestore();
 
-$('#submit').click(() => {
-    database.collection('Employees').doc().add({
+$('#submit').click(event => {
+    event.preventDefault();
+
+    database.collection('Employees').doc().set({
         Name: $('#name').val(),
         Role: $('#role').val(),
         StartDate: $('#start-date').val(),
         PayRate: $('#rate').val()
     })
+
+    refreshRows();
 })
+
+function refreshRows() {
+    $('.table-body').html('');
+    let row = 0;
+    database.collection('Employees').get().then(documents => {
+        documents.forEach(doc => {
+            let months = 0;
+            let total = months * doc.data().PayRate;
+            $(`<div class="row" id=${row}>`).append($('<div class="col-sm-2">').text(doc.data().Name), $('<div class="col-sm-2">').text(doc.data().Role), $('<div class="col-sm-2">').text(doc.data().StartDate), $('<div class="col-sm-2">').text(0), $('<div class="col-sm-2">').text(doc.data().PayRate), $('<div class="col-sm-2">').text(total)).appendTo('.table-body');
+        })
+    })
+}
